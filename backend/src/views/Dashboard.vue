a46181fbe54128f12d8c2c03bb6e51d9154fbf1b
TiagoRoccatagliata <tiagoroccatagliata@gmail.com>
2025-04-10 14:43:55 -0300

    Carpeta del backend (Que deberia ser adminPanel) Agregada

---
+++ backend/src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+ç<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "Login"
+})
+</script>
+
+<template>
+  <h1>Dashbaord</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ backend/src/views/Login.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "Login"
+})
+</script>
+
+<template>
+  <h1>Login Form</h1>
+</template>
+
+<style scoped>
+
+</style>
